9d7f572464d5e7b3ccb9f58cb03baa1c38e32b2a
DaFlusher <joyibe5@gmail.com>
2023-11-29 17:07:48 +0100

    form validation with a pageload delay of 10s

--- index.js
+++ index.js
@@ -43,17 +43,94 @@ cvc.addEventListener('input', function(){
     cvcnumber.textContent = cvc.value;
 })
 
-/**
- * 
- * const number = document.getElementById('cardnumber');
+//form validation with a pageload delay
+setTimeout(
+        function validateForm(){
 
-const month = document.getElementById('year');
+            let y = document.getElementById('cardname').value;
 
-const year = document.getElementById('month');
-const cvc = document.getElementById('cvc')
+            let textname;
+            if (y.length<6 ){
+                textname = 'Please write a valid name'
+                let message= document.getElementById('name-feedback')
+                message.style.color='rgb(179, 64, 64)'
+                
+            }
+            else{
+                textname="You're good to go"
+                let message= document.getElementById('name-feedback')
+                message.style.color='green'
+                
+            }
 
- */
+            let x = document.getElementById('cardnumber').value;
+
+            let text;
+            if (isNaN(x) || x.length !==12){
+                text= 'Wrong input, only twelve numbers allowed'
+                let message= document.getElementById('number-feedback')
+                message.style.color='rgb(179, 64, 64)'
+                
+                
+            }
+            else{
+                text = "You're good to go!"
+                let message= document.getElementById('number-feedback')
+                message.style.color='green'
+                
+            }
+
+            let z = document.getElementById('month').value;
+            let textmonth;
+
+            if (isNaN(z) || z.length !==2){
+                textmonth = "Can't be blank"
+                let message= document.getElementById('month-feedback')
+                message.style.color='rgb(179, 64, 64)'
+            }
+            else{
+                textmonth= 'valid'
+                let message= document.getElementById('month-feedback')
+                message.style.color='green'
+            };
             
+
+            let a = document.getElementById('year').value;
+            let textyear;
+
+            if(isNaN(a) || a.length <2 || a.length>2){
+                textyear = "Can't be blank"
+                let message= document.getElementById('year-feedback')
+                message.style.color='rgb(179, 64, 64)'
+            }
+
+            else{
+                textyear = 'valid'
+                let message= document.getElementById('year-feedback')
+                message.style.color='green'
+            }
+
+            let b = document.getElementById('cvc').value;
+            let textcvc;
+
+            if(isNaN(b) || b.length <3 || b.length>3){
+                textcvc = "Can't be blank, three numbers only"
+                let message= document.getElementById('cvc-feedback')
+                message.style.color='rgb(179, 64, 64)'
+            }
+            else{
+                textcvc= 'valid'
+                let message= document.getElementById('cvc-feedback')
+                message.style.color='green'
+            }
+
+            document.getElementById('name-feedback').textContent=textname;
+            document.getElementById('number-feedback').textContent=text;
+            document.getElementById('month-feedback').textContent=textmonth;
+            document.getElementById('year-feedback').textContent=textyear;
+            document.getElementById('cvc-feedback').textContent=textcvc;
+        },
+        10000);
 
 
 
