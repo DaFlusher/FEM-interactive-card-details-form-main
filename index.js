85e4ea56172b1ed591a71edf568fa77d5965453f
DaFlusher <joyibe5@gmail.com>
2023-11-29 03:11:49 +0100

    complete css, yet to perfect js

---
+++ index.js
@@ -0,0 +1,23 @@
+//name
+const cardname = document.getElementById('cardname');
+const jane = document.getElementById('jane');
+
+cardname.addEventListener('input', function(){
+    jane.textContent = cardname.value;
+});
+
+
+/**
+ * 
+ * const number = document.getElementById('cardnumber');
+
+const month = document.getElementById('year');
+
+const year = document.getElementById('month');
+const cvc = document.getElementById('cvc')
+
+ */
+            
+
+
+
